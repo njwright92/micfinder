0fe76e9576bab2d5bbfb9f0307ccbb0309928e90
njwright92 <njwright92@gmail.com>
2023-11-27 15:10:13 -0800

    "firestore and fb storage bucket, user profile"

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -3,30 +3,76 @@
 import React, { useState, useEffect, useContext } from "react";
 import Image from "next/image";
 import { EventContext } from "../components/eventContext";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { db } from "../../../firebase.config";
 
 export default function UserProfile() {
   const [profileImage, setProfileImage] = useState<File | null>(null);
-  const [name, setName] = useState<string>("");
-  const [bio, setBio] = useState<string>("");
-  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [profileImageUrl, setProfileImageUrl] = useState("");
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
   const { savedEvents } = useContext(EventContext);
+  const auth = getAuth();
+  const storage = getStorage();
 
   useEffect(() => {
-    // Load user data logic
-    setName("John Doe");
-    setBio("Comedy enthusiast and amateur stand-up comedian.");
-    // Load saved events and profile image similarly
-  }, []);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const userRef = doc(db, "users", user.uid);
+        getDoc(userRef)
+          .then((docSnap) => {
+            if (docSnap.exists()) {
+              const userData = docSnap.data();
+              setName(userData.name);
+              setBio(userData.bio);
+              setProfileImageUrl(userData.profileImageUrl);
+            }
+          })
+          .catch((error) => {
+            console.error("Error fetching user data:", error);
+          });
+      }
+    });
+  }, [auth]);
 
-  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      setProfileImage(event.target.files[0]);
+  const handleImageChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+      setProfileImage(file);
+
+      // Upload the file to Firebase Storage and get the URL
+      const storageRef = ref(storage, `profileImages/${auth.currentUser?.uid}`);
+      await uploadBytes(storageRef, file);
+      const url = await getDownloadURL(storageRef);
+      setProfileImageUrl(url);
     }
   };
 
-  const handleSubmit = () => {
-    console.log("Saving Profile Data:", { name, bio, profileImage });
-    setIsEditing(false);
+  const handleSubmit = async () => {
+    const user = auth.currentUser;
+    if (user) {
+      try {
+        const userRef = doc(db, "users", user.uid);
+        await setDoc(
+          userRef,
+          {
+            name,
+            bio,
+            profileImageUrl,
+          },
+          { merge: true }
+        ); // merge: true will update the document if it exists
+        setIsEditing(false);
+        console.log("Profile updated");
+      } catch (error) {
+        console.error("Error updating/creating profile:", error);
+      }
+    }
   };
 
   const handleEdit = () => {
@@ -44,22 +90,32 @@ export default function UserProfile() {
             Profile Picture:
           </label>
           {isEditing ? (
-            <input
-              id="profilePicture"
-              name="profilePicture"
-              type="file"
-              onChange={handleImageChange}
-              className="standard-input"
-            />
+            <>
+              <input
+                id="profilePicture"
+                name="profilePicture"
+                type="file"
+                onChange={handleImageChange}
+                className="standard-input"
+              />
+              {profileImage && (
+                <Image
+                  src={URL.createObjectURL(profileImage)}
+                  alt="Profile Preview"
+                  width={100}
+                  height={100}
+                  className="profile-image"
+                />
+              )}
+            </>
           ) : (
-            profileImage && (
+            profileImageUrl && (
               <Image
-                src={URL.createObjectURL(profileImage)}
-                alt="Profile"
+                src={profileImageUrl}
+                alt="Profile Preview"
                 width={100}
                 height={100}
-                objectFit="cover"
-                className="rounded-full"
+                className="profile-image"
               />
             )
           )}
