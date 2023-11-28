ebc09864791913d8f365f4d1672d38f4f3db57b1
njwright92 <njwright92@gmail.com>
2023-11-28 13:00:58 -0800

    "tailwind classes and css cleanup"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,37 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      borderRadius: {
+        'custom': '0.625rem'
+      },
+      colors: {
+        foreground: 'rgb(var(--foreground-rgb))',
+        backgroundStart: 'rgb(var(--background-start-rgb))',
+        backgroundEnd: 'rgb(var(--background-end-rgb))',
+        neuButtonBorder: '#ffffff',
+        modernInputBorder: '#c0c0c0',
+        modernInputBg: '#e5e0e0',
+        eventItemBorder: '#005eff',
+        modalContainerBg: '#f5f5f5',
+        modalContainerBorder: '#646262',
+        authButtonBg: '#000000',
+        closeButton: '#6b7280',
+        standardInputBorder: '#d1d5db',
+        standardInputBg: '#f9fafb',
+        theText: '#4b5563',
+        // Add other custom colors as needed
+      },
+      boxShadow: {
+        neuButton: '4px 4px 10px rgb(255, 255, 255), -4px -4px 10px rgb(255, 255, 255)',
+        // Add other custom shadows as needed
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        // Define a custom gradient
+        'gradient-to-b': 'linear-gradient(to bottom, var(--background-start-rgb), var(--background-end-rgb))',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;
