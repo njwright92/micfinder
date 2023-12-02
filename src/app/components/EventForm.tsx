dc61451a5d8fa73879e19b81508fa762f40ad7ff
njwright92 <njwright92@gmail.com>
2023-12-02 11:03:05 -0800

    "usememo callback, scroll modal and label update"

--- src/app/components/EventForm.tsx
+++ src/app/components/EventForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { db } from "../../../firebase.config";
@@ -39,40 +39,50 @@ const EventForm: React.FC = () => {
 
   const [formErrors, setFormErrors] = useState<string>("");
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    if (!event.name || !event.location || !event.date || !event.details) {
-      setFormErrors("Please fill in all required fields.");
-      return;
-    }
+  const memoizedEvent = useMemo(() => event, [event]);
 
-    setFormErrors(""); // Clear any previous errors
-    try {
-      await submitEvent(event);
-      setShowModal(false);
-      alert(
-        "Event has been added successfully! Check the events page to view!"
-      );
-    } catch (error) {
-      console.error("Error adding event: ", error);
-      setFormErrors("Failed to add the event. Please try again.");
-    }
-  };
+  const handleSubmit = useCallback(
+    async (e: React.FormEvent) => {
+      e.preventDefault();
+      if (
+        !memoizedEvent.name ||
+        !memoizedEvent.location ||
+        !memoizedEvent.date ||
+        !memoizedEvent.details
+      ) {
+        setFormErrors("Please fill in all required fields.");
+        return;
+      }
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
+      setFormErrors(""); // Clear any previous errors
+      try {
+        await submitEvent(memoizedEvent);
+        setShowModal(false);
+        alert(
+          "Event has been added successfully! Check the events page to view!"
+        );
+      } catch (error) {
+        console.error("Error adding event: ", error);
+        setFormErrors("Failed to add the event. Please try again.");
+      }
+    },
+    [memoizedEvent]
+  );
 
-    // Convert value to a number for 'lat' and 'lng' fields
-    if (name === "lat" || name === "lng") {
-      const numberValue = value === "" ? 0 : parseFloat(value);
-      setEvent({ ...event, [name]: numberValue });
-    } else {
-      setEvent({ ...event, [name]: value });
-    }
-  };
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      const { name, value } = e.target;
 
+      // Convert value to a number for 'lat' and 'lng' fields
+      if (name === "lat" || name === "lng") {
+        const numberValue = value === "" ? 0 : parseFloat(value);
+        setEvent((prevEvent) => ({ ...prevEvent, [name]: numberValue }));
+      } else {
+        setEvent((prevEvent) => ({ ...prevEvent, [name]: value }));
+      }
+    },
+    []
+  );
   return (
     <>
       <button
@@ -82,92 +92,86 @@ const EventForm: React.FC = () => {
         Add Event
       </button>
       <Modal show={showModal} onClose={() => setShowModal(false)}>
-        <form onSubmit={handleSubmit} className="form-container">
+        <form
+          onSubmit={handleSubmit}
+          className="form-container mx-auto overflow-auto max-h-screen"
+        >
           {formErrors && <p className="text-red-500">{formErrors}</p>}
-          <h1 className="text-2xl font-bold text-center text-black mt-4">
+          <h1 className="text-2xl font-bold text-center text-black mt-4 ">
             Add Event Form
           </h1>
           <p className="text-red-500 text-center mb-1">
             Please fill in all fields correctly. Then submit, and your event
             will be added!
           </p>
-          <div className="form-container">
-            <label htmlFor="eventName" className="the-text">
-              Event Name:
-            </label>
-            <input
-              type="text"
-              id="eventName"
-              name="name"
-              value={event.name}
-              onChange={handleChange}
-              className="standard-input"
-              required
-            />
-          </div>
+          <label htmlFor="eventName" className="the-text">
+            Event Name:
+          </label>
+          <input
+            type="text"
+            id="eventName"
+            name="name"
+            value={event.name}
+            onChange={handleChange}
+            className="standard-input"
+            required
+            autoComplete="off"
+          />
 
-          <div className="form-container">
-            <label htmlFor="location" className="the-text">
-              Location (Format: City State without comma):
-            </label>
-            <input
-              type="text"
-              id="location"
-              name="location"
-              value={event.location}
-              onChange={handleChange}
-              className="standard-input"
-              required
-            />
-          </div>
+          <label htmlFor="location" className="the-text">
+            Location **Needs City State without comma**:
+          </label>
+          <input
+            type="text"
+            id="location"
+            name="location"
+            value={event.location}
+            onChange={handleChange}
+            className="standard-input"
+            required
+          />
 
-          <div className="form-container">
-            <label htmlFor="details" className="the-text">
-              Details:
-            </label>
-            <textarea
-              id="details"
-              name="details"
-              value={event.details}
-              onChange={handleChange}
-              className="standard-input"
-              required
-            />
-          </div>
+          <label htmlFor="details" className="the-text">
+            Details:
+          </label>
+          <textarea
+            id="details"
+            name="details"
+            value={event.details}
+            onChange={handleChange}
+            className="standard-input"
+            rows={5}
+            required
+          />
 
-          <div className="form-container">
-            <label htmlFor="latitude" className="the-text">
-              Latitude (Get coordinates from Google Maps):
-            </label>
-            <input
-              type="number"
-              id="latitude"
-              name="lat"
-              value={event.lat}
-              onChange={handleChange}
-              className="standard-input"
-              required
-            />
-          </div>
+          <label htmlFor="latitude" className="the-text">
+            Latitude **Get coordinates from Google Maps**:
+          </label>
+          <input
+            type="number"
+            id="latitude"
+            name="lat"
+            value={event.lat}
+            onChange={handleChange}
+            className="standard-input"
+            required
+          />
 
-          <div className="form-container">
-            <label htmlFor="longitude" className="the-text">
-              Longitude (Get coordinates from Google Maps):
-            </label>
-            <input
-              type="number"
-              id="longitude"
-              name="lng"
-              value={event.lng}
-              onChange={handleChange}
-              className="standard-input"
-              required
-            />
-          </div>
-
-          <div className="form-container">
-            <label className="the-text">Is Recurring?:</label>
-            <div>
+          <label htmlFor="longitude" className="the-text">
+            Longitude **Get coordinates from Google Maps**:
+          </label>
+          <input
+            type="number"
+            id="longitude"
+            name="lng"
+            value={event.lng}
+            onChange={handleChange}
+            className="standard-input"
+            required
+          />
+          <h6 className="the-text">Is it a recurring event?:</h6>
+          <div className="flex flex-row">
+            <div className="flex flex-col items-center mr-4">
               <label htmlFor="isRecurringYes" className="the-text">
                 Yes
               </label>
@@ -179,7 +183,9 @@ const EventForm: React.FC = () => {
                 onChange={() => setEvent({ ...event, isRecurring: true })}
                 className="standard-input"
               />
+            </div>
 
+            <div className="flex flex-col items-center">
               <label htmlFor="isRecurringNo" className="the-text">
                 No
               </label>
@@ -194,18 +200,17 @@ const EventForm: React.FC = () => {
             </div>
           </div>
 
-          <div className="form-container">
-            <label htmlFor="date" className="the-text">
-              Date:
-            </label>
-            <DatePicker
-              id="date"
-              selected={event.date ? new Date(event.date) : null}
-              onChange={(date: Date | null) => setEvent({ ...event, date })}
-              placeholderText={`📅 ${new Date().toLocaleDateString()}`}
-              className="standard-input text-black"
-            />
-          </div>
+          <label htmlFor="date" className="the-text mt-2">
+            Date:
+          </label>
+          <DatePicker
+            id="date"
+            selected={event.date ? new Date(event.date) : null}
+            onChange={(date: Date | null) => setEvent({ ...event, date })}
+            placeholderText={`📅 ${new Date().toLocaleDateString()}`}
+            className="standard-input text-black"
+          />
+
           <button type="submit" className="neu-button py-1 px-2 mb-3">
             Submit Event
           </button>
