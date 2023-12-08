1352cb8021bcfc3f68f382f739f2e9f0f4c98be8
njwright92 <njwright92@gmail.com>
2023-12-08 12:46:47 -0800

    "seo updates"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import { EventProvider } from "./components/eventContext";
@@ -17,6 +18,32 @@ export default function RootLayout({
   return (
     <EventProvider>
       <html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content="Find open mic events near you" />
+          <meta
+            name="keywords"
+            content="Open Mic, Live Performances, Comedians, Poets, Musicians, Events Stand-up Comedy, Comics"
+          />
+          <title>OpenMicFinder</title>
+          <link
+            rel="icon"
+            href="/favicon.ico"
+            type="image/x-icon"
+            sizes="16x16"
+          />
+          <script type="application/ld+json">
+            {JSON.stringify({
+              "@context": "http://schema.org",
+              "@type": "Website",
+              name: "OpenMicFinder",
+              url: "https://micfinder-fb06f.web.app/",
+              description:
+                "Discover and share open mic events - your stepping stone in the world of stand-up comedy and live performances.",
+            })}
+          </script>
+        </Head>
         <body>
           <Header />
           {children}
